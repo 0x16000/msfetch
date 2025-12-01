6e3e1ec8a0a9603a32c734467c4a48b1d0186878
St1ryNight <robconn2009@proton.me>
2025-11-30 19:38:44 -0700

    fix openbsd support

--- src/mem.rs
+++ src/mem.rs
@@ -46,7 +46,7 @@ pub fn get_memory() -> u64 {
         / 1024
 }
 #[cfg(target_os = "openbsd")]
-fn get_memory() -> u64 {
+pub fn get_memory() -> u64 {
     let output = Command::new("sh")
         .arg("-c")
         .arg("sysctl hw.physmem")
@@ -55,7 +55,7 @@ fn get_memory() -> u64 {
     let mem_string = String::from_utf8(output.stdout).unwrap();
     mem_string
         .trim()
-        .split(": ")
+        .split("=")
         .nth(1)
         .unwrap()
         .parse::<u64>()
